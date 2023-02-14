1618fe657eeb1a3d106fbfd3bdd97edb1e3c39dd
Zdziszkee <zdziszkee@gmail.com>
2023-02-14 19:44:48 +0100

    started work on concurrent tracking of multiple wallets

--- src/main.rs
+++ src/main.rs
@@ -3,7 +3,7 @@ use std::fs::{File};
 use std::io::{Write};
 use std::time::Instant;
 use serde_json::{Value};
-use tokio::fs;
+use tokio::{fs, task, time};
 use std::time::SystemTime;
 use dashmap::DashMap;
 
@@ -31,23 +31,34 @@ async fn main() -> Result<(), Box<dyn std::error::Error>> {
     let contents: String = fs::read_to_string(config_path).await?;
     let wallets: Vec<String> = serde_json::from_str(&contents)?;
     for wallet in wallets {
-       // cache.insert(wallet, String::from(""));
-    }
+        // cache.insert(wallet, String::from(""));
 
+        loop {
+            let copy = wallet.clone();
+            // Spawn a new task to run the repeating code asynchronously.
+            let handle = task::spawn(async move {
+                let request = format!("https://api.etherscan.io/api?module=account&action=txlist&address={}&startblock=0&endblock=99999999&page=1&offset=10&sort=desc&apikey=NYA5ZPRQDECFZBINXU3GE6BTQQVWZN3FHG", copy);
+                let response: Value = reqwest::get(request)
+                    .await.unwrap()
+                    .json()
+                    .await.unwrap();
 
-    let response: Value = reqwest::get("https://api.etherscan.io/api?module=account&action=txlist&address=0xa7b5ca022774bd02842932e4358ddcbea0ccaade&startblock=0&endblock=99999999&page=1&offset=10&sort=desc&apikey=NYA5ZPRQDECFZBINXU3GE6BTQQVWZN3FHG")
-        .await?
-        .json()
-        .await?;
-    let transactions: &Vec<Value> = response["result"].as_array().unwrap();
-    for transaction in transactions {
-        let time_stamp = &transaction["timeStamp"].as_str();
-        let date_time = SystemTime::UNIX_EPOCH + std::time::Duration::from_secs(time_stamp.unwrap().parse().unwrap());
-        let formatted = format!("{}", chrono::DateTime::<chrono::Local>::from(date_time));
-        println!("{}", formatted);
+                let transactions: &Vec<Value> = response["result"].as_array().unwrap();
+
+                for transaction in transactions {
+                    let time_stamp = &transaction["timeStamp"].as_str();
+                    let date_time = SystemTime::UNIX_EPOCH + time::Duration::from_secs(time_stamp.unwrap().parse().unwrap());
+                    let formatted = format!("{}", chrono::DateTime::<chrono::Local>::from(date_time));
+                    println!("{}", formatted);
+                }
+
+                time::sleep(time::Duration::from_secs(180)).await;
+            });
+
+            // Wait for the task to complete.
+            handle.await.unwrap();
+        }
     }
-    let duration = start.elapsed();
-    println!("Operation took {} miliseconds to complete", duration.as_millis());
 
     Ok(())
 }
