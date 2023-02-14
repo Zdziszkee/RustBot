bdd3c848bf3da1f93102bb3dc7a8660701f7351d
Zdziszkee <zdziszkee@gmail.com>
2023-02-14 04:18:48 +0100

    shit starting to work

--- src/main.rs
+++ src/main.rs
@@ -1,22 +1,28 @@
-use std::collections::HashMap;
 use std::env;
-use std::fs::{File, OpenOptions};
-use std::io::{Read, Write};
+use std::fs::{File};
+use std::io::{Write};
+use std::time::Instant;
 use serde::{Serialize, Deserialize};
+use serde_json::{Value};
+use tokio::fs;
 
+#[derive(Clone)]
 #[derive(Serialize, Deserialize, Debug)]
 struct Transaction {
     id: String,
 }
 
+#[derive(Clone)]
 #[derive(Serialize, Deserialize, Debug)]
-struct Wallet {
+struct Wallet<> {
     id: String,
     transactions: Vec<Transaction>,
 }
 
 #[tokio::main]
 async fn main() -> Result<(), Box<dyn std::error::Error>> {
+    let start = Instant::now();
+
     let mut config_path = env::current_dir()?;
     config_path.push("botconfig.json");
 
@@ -24,22 +30,27 @@ async fn main() -> Result<(), Box<dyn std::error::Error>> {
         Ok(file) => file,
         Err(_) => {
             let mut new_file = File::create(&config_path).unwrap();
-            let example_transaction = Transaction { id: String::from("exampleTransactionId") };
-            let example_wallet = Wallet { id: String::from("examplewalletid"), transactions: vec![example_transaction] };
-            let serialized = serde_json::to_string(&example_wallet).unwrap();
+            let wallets = vec!["walletid1", "walletid2", "walletid3"];
+            let serialized = serde_json::to_string(&wallets).unwrap();
             new_file.write_all(serialized.as_ref())?;
             new_file
         }
     };
-
-    let mut contents = String::new();
-    config.read_to_string(&mut contents).unwrap();
-
-
-    let response = reqwest::get("https://api.etherscan.io/api?module=account&action=balance&address=0xde0b295669a9fd93d5f28d9ec85e40f4cb697bae&tag=latest&apikey=NYA5ZPRQDECFZBINXU3GE6BTQQVWZN3FHG")
+    let contents: String = fs::read_to_string(config_path).await?;
+    let array: Vec<String> = serde_json::from_str(&contents)?;
+    for x in array {
+        println!("{}", x)
+    }
+    let response: Value = reqwest::get("https://api.etherscan.io/api?module=account&action=txlist&address=0xc5102fE9359FD9a28f877a67E36B0F050d81a3CC&startblock=0&endblock=99999999&page=1&offset=10&sort=asc&apikey=NYA5ZPRQDECFZBINXU3GE6BTQQVWZN3FHG")
         .await?
-        .json::<HashMap<String, String>>()
+        .json()
         .await?;
-    println!("{:#?}", response);
+
+    let transactions: &Vec<Value> = response["result"].as_array().unwrap();
+    for transaction in transactions {
+        println!("{}", transaction)
+    }
+    let duration = start.elapsed();
+    println!("Operation took {} miliseconds to complete", duration.as_millis());
     Ok(())
 }
