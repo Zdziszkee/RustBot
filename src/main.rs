9286c39d7c470f274f8a478400c5132b49c5cdd0
Zdziszkee <zdziszkee@gmail.com>
2023-02-14 18:30:23 +0100

    added printing timestamp of transactions

--- src/main.rs
+++ src/main.rs
@@ -1,32 +1,23 @@
+use std::collections::HashMap;
 use std::env;
 use std::fs::{File};
 use std::io::{Write};
 use std::time::Instant;
 use serde::{Serialize, Deserialize};
-use serde_json::{Value};
+use serde_json::{json, Value};
 use tokio::fs;
-
-#[derive(Clone)]
-#[derive(Serialize, Deserialize, Debug)]
-struct Transaction {
-    id: String,
-}
-
-#[derive(Clone)]
-#[derive(Serialize, Deserialize, Debug)]
-struct Wallet<> {
-    id: String,
-    transactions: Vec<Transaction>,
-}
+use std::time::SystemTime;
 
 #[tokio::main]
 async fn main() -> Result<(), Box<dyn std::error::Error>> {
+    let mut cache: HashMap<String, i32> = HashMap::new();
+
     let start = Instant::now();
 
     let mut config_path = env::current_dir()?;
     config_path.push("botconfig.json");
 
-    let mut config = match File::open(&config_path) {
+    match File::open(&config_path) {
         Ok(file) => file,
         Err(_) => {
             let mut new_file = File::create(&config_path).unwrap();
@@ -36,21 +27,26 @@ async fn main() -> Result<(), Box<dyn std::error::Error>> {
             new_file
         }
     };
+
     let contents: String = fs::read_to_string(config_path).await?;
-    let array: Vec<String> = serde_json::from_str(&contents)?;
-    for x in array {
-        println!("{}", x)
+    let wallets: Vec<String> = serde_json::from_str(&contents)?;
+    for wallet in wallets {
+        cache.insert(wallet, 0);
     }
-    let response: Value = reqwest::get("https://api.etherscan.io/api?module=account&action=txlist&address=0xc5102fE9359FD9a28f877a67E36B0F050d81a3CC&startblock=0&endblock=99999999&page=1&offset=10&sort=asc&apikey=NYA5ZPRQDECFZBINXU3GE6BTQQVWZN3FHG")
+
+    let response: Value = reqwest::get("https://api.etherscan.io/api?module=account&action=txlist&address=0xa7b5ca022774bd02842932e4358ddcbea0ccaade&startblock=0&endblock=99999999&page=1&offset=10&sort=desc&apikey=NYA5ZPRQDECFZBINXU3GE6BTQQVWZN3FHG")
         .await?
         .json()
         .await?;
-
     let transactions: &Vec<Value> = response["result"].as_array().unwrap();
     for transaction in transactions {
-        println!("{}", transaction)
+        let time_stamp = &transaction["timeStamp"].as_str();
+        let date_time = SystemTime::UNIX_EPOCH + std::time::Duration::from_secs(time_stamp.unwrap().parse().unwrap());
+        let formatted = format!("{}", chrono::DateTime::<chrono::Local>::from(date_time));
+        println!("{}", formatted);
     }
     let duration = start.elapsed();
     println!("Operation took {} miliseconds to complete", duration.as_millis());
+
     Ok(())
 }
